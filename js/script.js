8e86c193e69ea55a06342039c43e91ee68474ff4
Arthur Lobanov <38422099+Arthur0403@users.noreply.github.com>
2019-08-23 12:41:00 +0300

    Newer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
-"use strict";function checkFooterForm(){var e=document.querySelector(".footer__form-input");document.querySelector(".footer__form-button").disabled=!e.value&&"disabled"}
+'use strict';
+
+const linkLogin     = document.querySelectorAll('.link-login'),
+			popupWrapper  = document.querySelector('.modal-wrapper'),
+			popupLogin    = document.querySelector('.modal--login'),
+			linkRecovery  = popupLogin.querySelector('.link-recovery'),
+			popupRecovery = document.querySelector('.modal--recovery'),
+			footerEmail   = document.querySelector('.footer__form-input');
+
+function checkFooterForm() {
+	document.querySelector('.footer__form-button').disabled = footerEmail.value ? false : "disabled";
+}
+
+for (let i = 0; i < linkLogin.length; i++) {
+    let link = linkLogin[i];
+    link.addEventListener('click', function (event) {
+		event.preventDefault();
+		event.preventDefault();
+	if (popupRecovery.classList.contains('modal--show')) {
+			popupRecovery.classList.remove('modal--show');
+			popupWrapper.classList.remove('modal--show');
+	}
+	popupWrapper.classList.add('modal--show');
+	popupLogin.classList.add('modal--show');
+
+    });
+}
+
+linkRecovery.addEventListener('click', function(event) {
+	event.preventDefault();
+	if (popupLogin.classList.contains('modal--show')) {
+			popupLogin.classList.remove('modal--show');
+			popupWrapper.classList.remove('modal--show');
+	}
+	popupWrapper.classList.add('modal--show');
+	popupRecovery.classList.add('modal--show');
+});
+
+popupWrapper.addEventListener("click", function() {
+	if (popupLogin.classList.contains('modal--show')) {
+			popupWrapper.classList.remove('modal--show');
+			popupLogin.classList.remove('modal--show');
+	}
+	if (popupRecovery.classList.contains('modal--show')) {
+			popupWrapper.classList.remove('modal--show');
+			popupRecovery.classList.remove('modal--show');
+	}
+});
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popupLogin.classList.contains('modal--show')) {
+			popupWrapper.classList.remove('modal--show');
+			popupLogin.classList.remove('modal--show');
+		}
+		if (popupRecovery.classList.contains('modal--show')) {
+			popupWrapper.classList.remove('modal--show');
+			popupRecovery.classList.remove('modal--show');
+		}
+	}
+});
